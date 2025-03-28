78b1bdf854084ad8e99f2f30117d84010125e37d
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-28 23:19:43 +0800

    added user input

--- print_in_lower_case.py
+++ print_in_lower_case.py
@@ -1,4 +1,6 @@
 # Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 
 # Ask user to input string in random cases
+string = input("Input string in random letter cases: ")
+
 # Print input in lower case
