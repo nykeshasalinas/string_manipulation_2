8fe5bdde91d9ab86370f42d023c09727e394bfd3
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-28 23:30:11 +0800

    added user input

--- check_if_upper_case.py
+++ check_if_upper_case.py
@@ -1,5 +1,7 @@
 # Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
 
 # Ask user to input string
+string = input("Input string: ")
+
 # Print "True" if all characters is on upper case
 # else print "false"
