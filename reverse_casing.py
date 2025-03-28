5ca9a3852a7921634d79538d047345a59c69f5dd
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-29 00:21:50 +0800

    added user input

--- reverse_casing.py
+++ reverse_casing.py
@@ -1,4 +1,6 @@
 # Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 
 # Ask user to input string in random letter cases
+string = input("Input string in random letter cases: ")
+
 # Print input in reversed letter cases
