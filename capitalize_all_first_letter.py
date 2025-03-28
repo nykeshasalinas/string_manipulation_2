e693d7a7302dca4eaf4d0aa08d24c07933c6d6d9
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-29 00:31:14 +0800

    added user input

--- capitalize_all_first_letter.py
+++ capitalize_all_first_letter.py
@@ -1,4 +1,6 @@
 # Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 
 # Ask user to input string
+string = input("Enter the string: ")
+
 # Print input in title casing
