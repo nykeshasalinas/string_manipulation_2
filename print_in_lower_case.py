e6e4effe6e4cace62f7efc17497a59a96efc2cdd
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-28 23:25:31 +0800

    prints input in lower case

--- print_in_lower_case.py
+++ print_in_lower_case.py
@@ -4,3 +4,11 @@
 string = input("Input string in random letter cases: ")
 
 # Print input in lower case
+lowercase_string = ""
+for char in string:
+    if 'A' <= char <= 'Z':
+        lowercase_string += chr(ord(char) + 32)
+    else:
+        lowercase_string += char
+
+print("String in lowercase:", lowercase_string)
