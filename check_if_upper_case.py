a2dc2f6788705a7591a3bbd6055d09aef0b4b149
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-28 23:41:24 +0800

    revised pseudocode and output

--- check_if_upper_case.py
+++ check_if_upper_case.py
@@ -5,10 +5,10 @@ string = input("Input string: ")
 
 for char in string:
     if 'a' <= char <= 'z':
-        # Print "All are upper case" if there is a lowercase letter
-        print("All are upper case")
+        # Print "Not all are upper case" if there is a lowercase letter
+        print("Not all are upper case")
         break
     else:
-        # Print "Not all are upper case" if all characters is on upper case
-        print("Not all are upper case")
+        # Print "All are upper case" if all characters is on upper case
+        print("All are upper case")
         break
