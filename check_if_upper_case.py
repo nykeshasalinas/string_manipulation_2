50d77292d82520cd05c71adfc228624d55b2a1c6
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-28 23:29:45 +0800

    added pseudocode

--- check_if_upper_case.py
+++ check_if_upper_case.py
@@ -1,1 +1,5 @@
 # Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+
+# Ask user to input string
+# Print "True" if all characters is on upper case
+# else print "false"
