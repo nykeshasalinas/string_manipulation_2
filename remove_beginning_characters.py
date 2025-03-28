ce095fab2e92549ef7f2b500375b813c4d529305
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-28 22:43:25 +0800

    added pseudocode

--- remove_beginning_characters.py
+++ remove_beginning_characters.py
@@ -1,1 +1,4 @@
 # Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+
+# Ask user to input string with space characters at the beginning
+# Print without the space characters using strip() method
