6d34fee107dc040efd711cdd0fc5dab99f3f8060
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-29 00:16:43 +0800

    added user inputs

--- center_string.py
+++ center_string.py
@@ -1,5 +1,9 @@
 # Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 
 # Ask user to input string
+string = input("Enter the string: ")
+
 # Ask user to input total length of string
+length = int(input("Enter the total length of the string: "))
+
 # Print input in the center
