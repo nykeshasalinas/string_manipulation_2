9efc69f28986f7dcb767ed46e5945d364b2959f3
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-29 00:01:47 +0800

    added pseudocode

--- add_space_characters_at_the_end.py
+++ add_space_characters_at_the_end.py
@@ -1,1 +1,5 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+# Ask user to input string
+# Ask user to input length of space to add at the end of string
+# Print input with desired spaces
