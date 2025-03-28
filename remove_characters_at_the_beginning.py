d6a0a58879b8fe5ad7c8ff5c25eee092f76e21fc
Nykesha Salinas <salinasnykesha12@gmail.com>
2025-03-29 00:33:30 +0800

    Delete remove_characters_at_the_beginning.py

--- remove_characters_at_the_beginning.py
+++
@@ -1,9 +0,0 @@
-# Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
-
-# Ask user to input string
-string = input("Input string: ")
-
-# Ask user to input the prefix to be removed
-prefix = input("Input prefix to be removed: ")
-
-# Print input without the prefix
