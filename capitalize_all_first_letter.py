4bc6a4de768270b487ef367aa1cc319844ea457d
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-29 00:30:54 +0800

    added pseudocode

--- capitalize_all_first_letter.py
+++ capitalize_all_first_letter.py
@@ -1,1 +1,4 @@
 # Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+
+# Ask user to input string
+# Print input in title casing
