3e0ee2c5312ad91abe9648dacd8b588ca70d3c1b
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-28 23:50:43 +0800

    added user inputs

--- check_if_string_end_with.py
+++ check_if_string_end_with.py
@@ -1,6 +1,10 @@
 # Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 
 # Ask user to input string
+string = input("Input string: ")
+
 # Ask user to input parameter
+parameter = input("Input parameter to check: ")
+
 # Print "String ends with parameter" if string ends with input parameter
 # Print "String does not end with parameter" if string does not end with input parameter
