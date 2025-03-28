821e9a36aa3aa7f952617724b37ced0486d3b860
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-29 00:23:05 +0800

    prints input in reverse casing

--- reverse_casing.py
+++ reverse_casing.py
@@ -4,3 +4,14 @@
 string = input("Input string in random letter cases: ")
 
 # Print input in reversed letter cases
+swapped_string = ""
+
+for char in string:
+    if 'a' <= char <= 'z':
+        swapped_string += chr(ord(char) - 32)
+    elif 'A' <= char <= 'Z':
+        swapped_string += chr(ord(char) + 32)
+    else:
+        swapped_string += char
+
+print("String after reversing letter case:", swapped_string)
