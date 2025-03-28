b332a66521f35e80621a07d79638915f225e1ed6
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-28 23:17:50 +0800

    added pseudocode

--- print_in_lower_case.py
+++ print_in_lower_case.py
@@ -1,1 +1,4 @@
 # Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+# Ask user to input string in random cases
+# Print input in lower case
