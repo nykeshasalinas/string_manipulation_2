d9b194bb49e6aba6d852c7fe080c6b4225fefddf
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-31 00:17:10 +0800

    added user input

--- batch_7/remove_ending_characters.py
+++ batch_7/remove_ending_characters.py
@@ -1,4 +1,6 @@
 # Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 
 # Ask user to input string with space characters at the end
+string = input("Input string with multiple spaces at the end: ")
+
 # Print without the space characters at the end
