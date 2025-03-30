275912dca9c0c76e586e151d132edd06dc07b540
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-31 00:48:28 +0800

    added pseudocode

--- batch_7/print_in_upper_case.py
+++ batch_7/print_in_upper_case.py
@@ -1,1 +1,4 @@
 # Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+
+# Ask user to input string in random cases
+# Print input in upper case
