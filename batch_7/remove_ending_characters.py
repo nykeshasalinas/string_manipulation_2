931dfee39db0340c7151d1dabd25ff3962adb87a
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-31 00:16:35 +0800

    added pseudocode

--- batch_7/remove_ending_characters.py
+++ batch_7/remove_ending_characters.py
@@ -1,1 +1,4 @@
 # Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
+# Ask user to input string with space characters at the end
+# Print without the space characters at the end
