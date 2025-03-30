ef76cf1a6cf36ddf6248e77c64ef5bc91b07ff71
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-31 00:48:47 +0800

    added user input

--- batch_7/print_in_upper_case.py
+++ batch_7/print_in_upper_case.py
@@ -1,4 +1,6 @@
 # Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 
 # Ask user to input string in random cases
+string = input("Input string in random letter cases: ")
+
 # Print input in upper case
