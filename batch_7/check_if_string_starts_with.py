14f6e157c826495fa421159ffe7af578a0115504
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-31 23:03:50 +0800

    added pseudocode

--- batch_7/check_if_string_starts_with.py
+++ batch_7/check_if_string_starts_with.py
@@ -1,1 +1,6 @@
 # Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+# Ask user to input string
+# Ask user to input parameter
+# Print "String starts with parameter" if string starts with input parameter
+# Print "String does not start with parameter" if string does not start with input parameter
