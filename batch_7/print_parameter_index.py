fdc519297b73e3f7f9371fc2977c82a9c6ed89d9
nykeshasalinas <salinasnykesha12@gmail.com>
2025-04-01 00:17:36 +0800

    revised pseudocode

--- batch_7/print_parameter_index.py
+++ batch_7/print_parameter_index.py
@@ -1,5 +1,8 @@
 # Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 
 # Ask user to input string
+string = input("Enter the string: ")
+
+# Ask user to input parameter to find
 # Check first location of parameter in string
 # Print index of the parameter
