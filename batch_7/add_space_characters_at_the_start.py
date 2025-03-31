002cfad2d40adb16f1254237e9f4c5f64db766ed
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-31 23:14:35 +0800

    added pseudocode

--- batch_7/add_space_characters_at_the_start.py
+++ batch_7/add_space_characters_at_the_start.py
@@ -1,1 +1,5 @@
 # Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+
+# Ask user to input string
+# Ask user to input length of space to add at the start of string
+# Print input with desired spaces
