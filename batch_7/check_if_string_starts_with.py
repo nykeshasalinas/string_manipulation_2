8440ed28d5f564f4999a1dd894afcaa7f04affff
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-31 23:07:17 +0800

    checks if input starts with parameter

--- batch_7/check_if_string_starts_with.py
+++ batch_7/check_if_string_starts_with.py
@@ -6,5 +6,10 @@ string = input("Input string: ")
 # Ask user to input parameter
 parameter = input("Input parameter to check: ")
 
-# Print "String starts with parameter" if string starts with input parameter
-# Print "String does not start with parameter" if string does not start with input parameter
+# Check if input starts with given parameter
+if string[:len(parameter)] == parameter:
+    # Print "String starts with parameter" if string starts with input parameter
+    print("String starts with parameter")
+else:
+    # Print "String does not start with parameter" if string does not start with input parameter
+    print("String does not start with parameter")
