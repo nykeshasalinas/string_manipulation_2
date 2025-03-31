9d2ff63b37f86e7edaf36546bc3a7c15c1626b2a
nykeshasalinas <salinasnykesha12@gmail.com>
2025-04-01 00:01:10 +0800

    added user inputs

--- batch_7/count_given_parameter.py
+++ batch_7/count_given_parameter.py
@@ -1,6 +1,10 @@
 # Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 
 # Ask user to input string
+string = input("Input string: ")
+
 # Ask user to input parameter to count
+parameter = input("Input parameter to count: ")
+
 # Count how many times parameter appeared in the string
 # Print count of the parameter
