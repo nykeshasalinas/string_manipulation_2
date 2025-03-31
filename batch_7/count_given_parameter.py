217d976f7f7acc4ea5079e81685642561c350ce3
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-31 23:56:25 +0800

    added pseudocode

--- batch_7/count_given_parameter.py
+++ batch_7/count_given_parameter.py
@@ -1,1 +1,6 @@
 # Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+# Ask user to input string
+# Ask user to input parameter to count
+# Count how many times parameter appeared in the string
+# Print count of the parameter
