eaddd67521deb81332d7abe0959b9905e4d6bb9a
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-31 23:04:15 +0800

    added user inputs

--- batch_7/check_if_string_starts_with.py
+++ batch_7/check_if_string_starts_with.py
@@ -1,6 +1,10 @@
 # Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 
 # Ask user to input string
+string = input("Input string: ")
+
 # Ask user to input parameter
+parameter = input("Input parameter to check: ")
+
 # Print "String starts with parameter" if string starts with input parameter
 # Print "String does not start with parameter" if string does not start with input parameter
