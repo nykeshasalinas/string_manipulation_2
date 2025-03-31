ec0033613577f352018193ea05a9dc17fed7122b
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-31 22:56:38 +0800

    checks if input is all on lower case

--- batch_7/check_if_lower_case.py
+++ batch_7/check_if_lower_case.py
@@ -3,5 +3,16 @@
 # Ask user to input string
 string = input("Input string: ")
 
+is_lower = True
+
+for char in string:
+    if 'A' <= char <= 'Z':
+        is_lower = False
+        break
+
 # Print "Not all are lower case" if there is an upper case letter
+if is_lower == False:
+    print("Not all are lower case")
 # Print "All are lower case" if all characters is on lower case
+else:
+    print("All are lower case")
