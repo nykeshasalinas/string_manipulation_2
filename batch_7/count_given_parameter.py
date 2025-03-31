c36f5efc1176821562c3df7279bac6a18c836c24
nykeshasalinas <salinasnykesha12@gmail.com>
2025-04-01 00:05:59 +0800

    counts how many times parameter appeared in string

--- batch_7/count_given_parameter.py
+++ batch_7/count_given_parameter.py
@@ -11,4 +11,9 @@ parameter_count = 0
 index = 0
 
 # Count how many times parameter appeared in the string
+while index <= len(string) - len(parameter):
+    if string[index:index + len(parameter)] == parameter:
+        parameter_count += 1
+    index += 1
+
 # Print count of the parameter
