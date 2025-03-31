3870e16117e9ad7085ecefb20945164da078214e
nykeshasalinas <salinasnykesha12@gmail.com>
2025-04-01 00:30:00 +0800

    revised user inputs

--- batch_7/print_parameter_index.py
+++ batch_7/print_parameter_index.py
@@ -1,10 +1,10 @@
 # Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 
 # Ask user to input string
-string = input("Enter the string: ")
+string = input("Input string: ")
 
 # Ask user to input parameter to find
-parameter = input("Enter the substring to find: ")
+parameter = input("Input parameter to find: ")
 
 # Check first location of parameter in string
 found = False
